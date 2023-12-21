03c0d9fbf8e6cbe1aaa5a3c22b16af0766267380
Joseph Bautista <josephreuben.bautista@gmail.com>
2023-12-21 17:24:33 +0800

    initial commit

---
+++ app/Services/Task/Models/Task.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services\Task\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+}
